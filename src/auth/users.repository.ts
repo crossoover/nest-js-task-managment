5d9afe1ae26e10dc7deb5d605ca713a0a34a8a58
Viacheslav Savratsky <savratskywork@gmail.com>
2021-07-26 23:45:39 +0300

    5.2 Setting up AuthModule

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {}
