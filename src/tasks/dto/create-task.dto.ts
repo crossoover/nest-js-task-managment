156e8bc51748116b593cc3a19a6082f685577dc5
Viacheslav Savratsky <savratskywork@gmail.com>
2021-07-26 11:39:36 +0300

    2.2 ValidationPipe Creating a task

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,12 +1,20 @@
 /* eslint-disable prettier/prettier */
 import { TasksStatus } from '../task.model';
-
+import { IsNotEmpty } from 'class-validator';
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
 export class UpdateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
+  
+  @IsNotEmpty()
   status: TasksStatus;
 }
