60c8befa49295994064312bef14ec58de78ba5bb
Viacheslav Savratsky <savratskywork@gmail.com>
2021-07-27 17:53:31 +0300

    6.8 Restricting deleting a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -22,7 +22,7 @@ export class TasksService {
   }
 
   async getTaskById(id: string, user: User): Promise<Task> {
-    const found = await this.tasksRepository.findOne({ where: { id, user } });
+    const found = await this.tasksRepository.findOne( { id, user } );
 
     if (!found) {
       throw new NotFoundException(`Task with id "${id}" does not exist.`);
@@ -34,8 +34,9 @@ export class TasksService {
     return this.tasksRepository.createTask(createTaskDto, user);
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete(id);
+  async deleteTask(id: string, user: User): Promise<void> {    
+    const result = await this.tasksRepository.delete({ id, user });
+
     if (result.affected === 0) {
       throw new NotFoundException(`Task with id "${id}" does not exist.`);
     }
