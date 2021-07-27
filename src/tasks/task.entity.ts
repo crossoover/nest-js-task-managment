bc8c8f277338f2367304c2448a4873e807787fb3
Viacheslav Savratsky <savratskywork@gmail.com>
2021-07-27 17:21:36 +0300

    6.3 Serialize user data

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+import { Exclude } from 'class-transformer';
 import { User } from 'src/auth/user.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TasksStatus } from './task-status.enum';
@@ -18,5 +19,6 @@ export class Task {
   status: TasksStatus;
 
   @ManyToOne(_type => User, user => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
