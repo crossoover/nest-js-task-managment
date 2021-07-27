fa01eeb6d0125be9057d85638668f2d6383d9016
Viacheslav Savratsky <savratskywork@gmail.com>
2021-07-27 15:15:29 +0300

    5.8 Feature Signing in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,8 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
-
+import * as bcrypt from 'bcrypt';
+import { UnauthorizedException } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   constructor(
@@ -13,5 +14,15 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const {username, password} = authCredentialsDto;
+    const user = await this.usersRepository.findOne({username});
+
+    if(user && (await bcrypt.compare(password,user.password))){
+      return 'success';
+    }
+    throw new UnauthorizedException('Please check your login credentials.');
   }
 }
